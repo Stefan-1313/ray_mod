8c719fb9d9c641f25e9540ef58484b73478da391
Stefan van der Kleij <s.vanderkleij@viroteq.com>
2022-07-11 18:34:11 +0200

    Added feature to use observation during exploration.

--- rllib/utils/exploration/exploration.py
+++ rllib/utils/exploration/exploration.py
@@ -86,6 +86,7 @@ class Exploration:
     @DeveloperAPI
     def get_exploration_action(self,
                                *,
+                               input_dict: Dict[str, TensorType],  # ADDED
                                action_distribution: ActionDistribution,
                                timestep: Union[TensorType, int],
                                explore: bool = True):
